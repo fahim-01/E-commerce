90ddf47daba9e882553195b3ae7382a388a293eb
unknown <ffahimkhan960@gmail.com>
2020-09-26 19:23:27 +0600

    file urls updated

--- eshop/eshop/urls.py
+++ eshop/eshop/urls.py
@@ -20,6 +20,6 @@ from django.conf.urls.static import static
 from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('shop/', include('shop.urls')),
-     path('', views.index)
+    path('', include('shop.urls')),
+    # path('shop/', include('shop.urls')),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
